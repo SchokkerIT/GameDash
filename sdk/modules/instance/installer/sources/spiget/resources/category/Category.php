3d3691678ebbc6928b4594576e86faa25f9e6ad4
Jesse-Schokker <jesseschokker@live.nl>
2020-07-15 12:12:15 +0200

    update

--- sdk/modules/instance/installer/sources/spiget/resources/category/Category.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    namespace Electrum\Userland\Sdk\Module\Installed\Instance\Installer\Sources\Spiget\Resources\Category;
-
-    use \Electrum\Userland\Sdk\Module\Template;
-
-    class Category extends Template\Instance\Installer\Source\Category\Category {}
-
-?>
--- sdk/modules/instance/installer/sources/steamcmd/resources/resource/Version.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    namespace Electrum\Userland\Sdk\Module\Installed\Instance\Installer\Sources\SteamCmd\Resources\Resource;
-
-    use \Electrum\Userland\Sdk\Module\Template;
-
-    class Version extends Template\Instance\Installer\Source\Resource\Version {}
-
-?>
